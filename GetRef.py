09780f2e18cce56764237a7664423ef87182dcad
WeiJie Ou <ouweijie123@outlook.com>
2023-09-26 23:28:04 +0800

    modify GetRef to APA format

--- GetRef.py
+++ GetRef.py
@@ -3,12 +3,13 @@
 # @Author  : Euclid-Jie
 # @File    : GetRef.py
 import time
-
+import os
 import pandas as pd
 from bs4 import BeautifulSoup  # 导入类库
 import re
 from selenium.webdriver import Chrome, ChromeOptions  # 导入类库
 from selenium.webdriver.common.by import By
+from selenium import webdriver
 import tkinter as tk
 from tkinter import filedialog
 
@@ -22,30 +23,37 @@ def getLocalFile():
 
 
 def getNewRef(driver, OldRef_str):
-    baseUrl = 'https://scholar.google.com.hk/scholar?hl=zh-CN&q='
+    baseUrl = "https://scholar.google.com.hk/scholar?hl=zh-CN&q="
     try:
-        p = re.compile('(?<=\.).+(?=\[)')
+        p = re.compile("(?<=\.).+(?=\[)")
         Url = baseUrl + p.findall(OldRef_str)[0]
     except:
         Url = baseUrl + OldRef_str
     driver.get(Url)  # 跳转网址
-    driver.find_elements(By.CLASS_NAME, 'gs_or_cit.gs_or_btn.gs_nph')[0].find_elements(By.TAG_NAME, 'span')[0].click()
+    driver.find_elements(By.CLASS_NAME, "gs_or_cit.gs_or_btn.gs_nph")[0].find_elements(
+        By.TAG_NAME, "span"
+    )[0].click()
     time.sleep(1)
-    html = driver.find_elements(By.ID, 'gs_cit-bdy')[0].find_elements(By.TAG_NAME, 'tr')[0].get_attribute('outerHTML')
+    html = (
+        driver.find_elements(By.ID, "gs_cit-bdy")[0]
+        .find_elements(By.TAG_NAME, "tr")[2]
+        .get_attribute("outerHTML")
+    )
     soup = BeautifulSoup(html, features="lxml")
-    NewRef_str = soup.text.split('GB/T 7714')[1]
-    return NewRef_str
+    if "APA" in soup.text:
+        NewRef_str = soup.text.split("APA")[1]
+        return NewRef_str
+    else:
+        return ""
 
 
-if __name__ == '__main__':
-    option = ChromeOptions()  # 初始化类
-    option.add_experimental_option("excludeSwitches", ['enable-automation', 'enable-logging'])  # 添加参数
+if __name__ == "__main__":
+    options = webdriver.ChromeOptions()
     ip = "127.0.0.1"
-    # TODO 请查看代理软件Port
-    port = "4835"
+    port = "12345"
     # 设置代理
-    option.add_argument("--proxy-server=http://{}:{}".format(ip, port))
-    driver = Chrome(options=option)  # 模拟开浏览器
+    options.add_argument("--proxy-server=http://{}:{}".format(ip, port))
+    driver = Chrome(options=options)  # 模拟开浏览器
 
     filePath = getLocalFile()
     df = pd.read_csv(filePath)
